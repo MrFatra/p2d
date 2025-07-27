6deeb7611df60b42bdfd560ae5e2a1249124c55d
MrFatra <ramadhanfatra2004@gmail.com>
2025-07-27 21:22:25 +0700

    feat(model): add some missing attribute for toddler model

--- app/Models/Toddler.php
+++ app/Models/Toddler.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Toddler extends Model
 {
-    //
+    protected $table = 'toddlers';
+
+    protected $fillable = [
+        'user_id',
+        'weight',
+        'height',
+        'upper_arm_circumference',
+        'nutrition_status',
+        'vitamin_a',
+        'imunization_followup',
+        'food_supplement',
+        'parenting_education',
+        'motor_development',
+        'checkup_date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
