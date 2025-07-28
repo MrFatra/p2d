cc0d900cc8ae025ab5aa5af652d6c38eff5b0916
MrFatra <ramadhanfatra2004@gmail.com>
2025-07-28 16:34:19 +0700

    fix(resource): unused code commented

--- app/Filament/Resources/PermissionResource/Pages/ListPermissions.php
+++ app/Filament/Resources/PermissionResource/Pages/ListPermissions.php
@@ -1,19 +1,21 @@
 <?php
 
-namespace App\Filament\Resources\PermissionResource\Pages;
+// namespace App\Filament\Resources\PermissionResource\Pages;
 
-use App\Filament\Resources\PermissionResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
+// use App\Filament\Resources\PermissionResource;
+// use Filament\Actions;
+// use Filament\Resources\Pages\ListRecords;
 
-class ListPermissions extends ListRecords
-{
-    protected static string $resource = PermissionResource::class;
+// class ListPermissions extends ListRecords
+// {
+//     protected static string $resource = PermissionResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
-}
+//     protected function getHeaderActions(): array
+//     {
+//         return [
+//             Actions\CreateAction::make(),
+//         ];
+//     }
+// }
+
+?>
