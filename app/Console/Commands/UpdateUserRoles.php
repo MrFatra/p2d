57f46473b8e8079d418453b02a17627f4b0d25f0
MrFatra <ramadhanfatra2004@gmail.com>
2025-08-04 06:03:17 +0700

    feat(scheduler): add automatic update user role per day

---
+++ app/Console/Commands/UpdateUserRoles.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UpdateUserRoles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-user-roles';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update daily the users role.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info("Updating roles...");
+
+        User::chunk(100, function ($users) {
+            foreach ($users as $user) {
+                $newRole = User::determineTypeOfUser($user->birth_date);
+
+                if (!$user->hasRole($newRole)) {
+                    $user->syncRoles($newRole);
+                }
+            }
+        });
+
+        $this->info("Done updating roles.");
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('notify:posyandu')->dailyAt('07:00');
+        $schedule->command('user:update-roles')->daily();
     }
 
     /**
