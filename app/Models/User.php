bad110fde3cea2e85a13f7986bfcee4a70974615
MrFatra <ramadhanfatra2004@gmail.com>
2025-08-04 21:45:53 +0700

    fix(model): change query, field, & cardinality

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Carbon\Carbon;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
@@ -14,14 +12,8 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'family_card_number',
         'national_id',
@@ -30,27 +22,17 @@ class User extends Authenticatable implements FilamentUser
         'birth_date',
         'gender',
         'phone_number',
+        'rt',
+        'rw',
         'address',
-        'latitude',
-        'longitude',
         'hamlet',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -64,7 +46,9 @@ class User extends Authenticatable implements FilamentUser
         parent::boot();
 
         static::creating(function ($user) {
-            $user->syncRoles(self::determineTypeOfUser($user->birth_date));
+            if (!$user->hasRole(['admin', 'cadre'])) {
+                $user->syncRoles(self::determineTypeOfUser($user->birth_date));
+            }
         });
 
         static::updating(function ($user) {
@@ -72,7 +56,7 @@ class User extends Authenticatable implements FilamentUser
                 unset($user->password);
             }
 
-            if ($user->isDirty('birth_date') && !$user->hasRole('admin')) {
+            if ($user->isDirty('birth_date') && !$user->hasRole(['admin', 'cadre'])) {
                 $user->syncRoles(self::determineTypeOfUser($user->birth_date));
             }
         });
@@ -82,40 +66,60 @@ class User extends Authenticatable implements FilamentUser
     {
         $now = Carbon::now();
 
-        switch ($category) {
-            case 'baby':
-                return User::whereDate('birth_date', '>', $now->copy()->subYear())
-                    ->whereDate('birth_date', '<=', $now)
-                    // TODO: This query may be used to all roles...
-                    ->whereDoesntHave('infant', function ($query) use ($now) {
-                        $query->whereMonth('created_at', $now->month)
-                            ->whereYear('created_at', $now->year);
-                    })
-                    ->get();
+        $categories = [
+            'baby' => [
+                'min' => 0,
+                'max' => 1,
+                'relation' => 'infants',
+            ],
+            'toddler' => [
+                'min' => 1,
+                'max' => 5,
+                'relation' => 'toddlers',
+            ],
+            'teenager' => [
+                'min' => 12,
+                'max' => 18,
+                'relation' => 'teenagers',
+            ],
+            'elderly' => [
+                'min' => 60,
+                'max' => null,
+                'relation' => 'elderlies',
+            ],
+            'mother' => [
+                'min' => 12,
+                'max' => 60,
+                'relation' => 'pregnantPostpartumBreastfeedings',
+                'gender' => 'P',
+            ],
+        ];
 
-
-            case 'toddler':
-                return User::whereDate('birth_date', '<=', $now->copy()->subYear())
-                    ->whereDate('birth_date', '>', $now->copy()->subYears(5))
-                    ->get();
-
-            case 'teenager':
-                return User::whereDate('birth_date', '<=', $now->copy()->subYears(12))
-                    ->whereDate('birth_date', '>', $now->copy()->subYears(18))
-                    ->get();
-
-            case 'elderly':
-                return User::whereDate('birth_date', '<=', $now->copy()->subYears(60))->get();
-
-            case 'mother':
-                return User::where('gender', 'P')
-                    ->whereDate('birth_date', '<=', $now->copy()->subYears(12))
-                    ->whereDate('birth_date', '>', $now->copy()->subYears(60))
-                    ->get();
-
-            default:
-                return User::all();
+        if (!$category || !array_key_exists($category, $categories)) {
+            return User::all();
         }
+
+        $config = $categories[$category];
+        $query = User::query();
+
+        if (!empty($config['max'])) {
+            $query->whereDate('birth_date', '>', $now->copy()->subYears($config['max']));
+        }
+
+        if (!empty($config['min'])) {
+            $query->whereDate('birth_date', '<=', $now->copy()->subYears($config['min']));
+        }
+
+        if (isset($config['gender'])) {
+            $query->where('gender', $config['gender']);
+        }
+
+        $query->whereDoesntHave($config['relation'], function ($q) use ($now) {
+            $q->whereYear('created_at', $now->year)
+              ->whereMonth('created_at', $now->month);
+        });
+
+        return $query->get();
     }
 
     private static function determineTypeOfUser($userBirthDate)
@@ -154,31 +158,31 @@ class User extends Authenticatable implements FilamentUser
         return $this->hasRole('admin');
     }
 
-    // Relasi untuk data remaja
-    public function teenager()
+    public function infants()
     {
-        return $this->hasOne(Teenager::class);
+        return $this->hasMany(Infant::class);
     }
 
-    // Relasi untuk data bayi
-    public function infant()
+    public function toddlers()
     {
-        return $this->hasOne(Infant::class);
+        return $this->hasMany(Toddler::class);
     }
 
-    // Relasi untuk data ibu hamil/nifas/menyusui
-    public function pregnantPostpartumBreastfeeding()
+    public function teenagers()
     {
-        return $this->hasOne(PregnantPostpartumBreastfeending::class);
+        return $this->hasMany(Teenager::class);
     }
 
-    // Relasi untuk data lansia
-    public function elderly()
+    public function pregnantPostpartumBreastfeedings()
     {
-        return $this->hasOne(Elderly::class);
+        return $this->hasMany(PregnantPostpartumBreastfeending::class);
     }
 
-    // Relasi ke banyak laporan
+    public function elderlies()
+    {
+        return $this->hasMany(Elderly::class);
+    }
+
     public function reports()
     {
         return $this->hasMany(Report::class);
