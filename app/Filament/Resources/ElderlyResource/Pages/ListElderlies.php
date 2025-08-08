f618b5bf74d6bf20597700f97faf39ea76f2aeb7
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-08-08 22:54:33 +0700

    fixed export on resource pregnant and elderly

--- app/Filament/Resources/ElderlyResource/Pages/ListElderlies.php
+++ app/Filament/Resources/ElderlyResource/Pages/ListElderlies.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\ElderlyResource;
 use App\Filament\Resources\ElderlyResource\Widgets\ElderlyVisitsChart;
 use App\Filament\Resources\ElderlyResource\Widgets\StatsOverview;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
@@ -22,9 +23,38 @@ class ListElderlies extends ListRecords
             Actions\Action::make('export-excel')
                 ->label('Export Excel')
                 ->icon('heroicon-o-arrow-up-tray')
-                ->action(function () {
+                ->modalSubmitActionLabel('Export')
+                ->form([
+                    TextInput::make('month')
+                        ->type('month') // HTML5 input type
+                        ->label('Pilih Bulan')
+                        ->default(now()->format('Y-m')) // Default format yang valid: '2025-08'
+                        ->required()
+                    // \Filament\Forms\Components\DatePicker::make('month')
+                    //     ->label('Pilih Bulan')
+                    //     ->native(false)
+                    //     ->extraInputAttributes(['type' => 'month'])
+                    //     ->format('m/Y')
+                    //     ->displayFormat('F Y') // Format bulan & tahun
+                    //     ->required(),
+                ])
+                ->action(function (array $data) {
+                    $query = $this->getFilteredTableQuery();
+
+                    // Filter berdasarkan bulan terpilih
+                    if (!empty($data['month'])) {
+                        $month = \Carbon\Carbon::parse($data['month']);
+                        $query->whereMonth('created_at', $month->month)
+                            ->whereYear('created_at', $month->year);
+                    }
+
+                    $filteredData = $query->get();
+
                     return response()->streamDownload(
-                        fn() => print(\Maatwebsite\Excel\Facades\Excel::download(new \App\Exports\ElderlyExport, 'Lansia.xlsx')->getFile()->getContent()),
+                        fn() => print(\Maatwebsite\Excel\Facades\Excel::download(
+                            new \App\Exports\ElderlyExport($filteredData),
+                            'lansia.xlsx'
+                        )->getFile()->getContent()),
                         'laporan-list-data-lansia.xlsx'
                     );
                 }),
--- app/Filament/Resources/PregnantResource/Pages/ListPregnants.php
+++ app/Filament/Resources/PregnantResource/Pages/ListPregnants.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\PregnantResource;
 use App\Filament\Resources\PregnantResource\Widgets\PregnantVisitsChart;
 use App\Filament\Resources\PregnantResource\Widgets\VisitorOverview;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
@@ -22,12 +23,38 @@ class ListPregnants extends ListRecords
             Actions\Action::make('export-excel')
                 ->label('Export Excel')
                 ->icon('heroicon-o-arrow-up-tray')
-                ->action(function () {
+                ->modalSubmitActionLabel('Export')
+                ->form([
+                    TextInput::make('month')
+                        ->type('month') // HTML5 input type
+                        ->label('Pilih Bulan')
+                        ->default(now()->format('Y-m')) // Default format yang valid: '2025-08'
+                        ->required()
+                    // \Filament\Forms\Components\DatePicker::make('month')
+                    //     ->label('Pilih Bulan')
+                    //     ->native(false)
+                    //     ->extraInputAttributes(['type' => 'month'])
+                    //     ->format('m/Y')
+                    //     ->displayFormat('F Y') // Format bulan & tahun
+                    //     ->required(),
+                ])
+                ->action(function (array $data) {
                     $query = $this->getFilteredTableQuery();
-                    $data = $query->get();
+
+                    // Filter berdasarkan bulan terpilih
+                    if (!empty($data['month'])) {
+                        $month = \Carbon\Carbon::parse($data['month']);
+                        $query->whereMonth('created_at', $month->month)
+                            ->whereYear('created_at', $month->year);
+                    }
+
+                    $filteredData = $query->get();
 
                     return response()->streamDownload(
-                        fn() => print(\Maatwebsite\Excel\Facades\Excel::download(new \App\Exports\PregnantExport($data), 'ibu-hamil.xlsx')->getFile()->getContent()),
+                        fn() => print(\Maatwebsite\Excel\Facades\Excel::download(
+                            new \App\Exports\ElderlyExport($filteredData),
+                            'ibu-hamil.xlsx'
+                        )->getFile()->getContent()),
                         'laporan-list-data-ibu-hamil.xlsx'
                     );
                 }),
