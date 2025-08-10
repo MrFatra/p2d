94b70cf6840a97418b9f99889cfca1af474e2a6c
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-08-10 22:55:25 +0700

    add export on resource Report

--- app/Helpers/MonthlyReport.php
+++ app/Helpers/MonthlyReport.php
@@ -4,6 +4,8 @@ namespace App\Helpers;
 
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class MonthlyReport
 {
@@ -33,5 +35,14 @@ class MonthlyReport
         }
 
         return $data;
+    }
+
+    public static function getMonthlyReportByRecord(Model $record): array
+    {
+        $cacheKey = 'monthly-report-' . Carbon::parse($record->uploaded_at)->format('Y-m');
+
+        return Cache::remember($cacheKey, now()->addHours(12), function () use ($record) {
+            return (new MonthlyReport())->countPerModelByDate(Carbon::parse($record->uploaded_at));
+        });
     }
 }
