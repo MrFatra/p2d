6a6c8cc1609684bf5d0ceff1ec5f37a0574cdc01
MrFatra <ramadhanfatra2004@gmail.com>
2025-08-11 21:13:17 +0700

    feat: add excel template for inserting users

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -19,44 +19,62 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make()
+                ->icon('heroicon-o-plus-circle')
                 ->visible(fn() => auth()->user()->can('pengguna:create')),
-            Actions\Action::make('export-excel')
-                ->visible(fn() => auth()->user()->can('pengguna:export'))
-                ->label('Export Excel')
-                ->icon('heroicon-o-arrow-up-tray')
-                ->action(function () {
-                    $query = $this->getFilteredTableQuery();
-                    $data = $query->get();
 
-                    return response()->streamDownload(
-                        fn() => print(\Maatwebsite\Excel\Facades\Excel::download(new \App\Exports\UserExport($data), 'pengguna.xlsx')->getFile()->getContent()),
-                        'laporan-list-data-pengguna.xlsx'
-                    );
-                }),
-            Actions\Action::make('import-excel')
-                ->label('Import Excel')
-                ->icon('heroicon-o-arrow-down-tray')
-                ->visible(fn() => auth()->user()->can('pengguna:import'))
-                ->form([
-                    FileUpload::make('file')
-                        ->label('File Excel')
-                        ->disk('public')
-                        ->visibility("public")
-                        ->directory('imports/users')
-                        ->required()
-                        ->acceptedFileTypes(['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']),
-                ])
-                ->action(function (array $data): void {
+            Actions\ActionGroup::make([
+                Actions\Action::make('export-excel')
+                    ->visible(fn() => auth()->user()->can('pengguna:export'))
+                    ->label('Export Excel')
+                    ->icon('heroicon-o-arrow-up-tray')
+                    ->action(function () {
+                        $query = $this->getFilteredTableQuery();
+                        $data = $query->get();
 
-                    $file = public_path('storage/' . $data['file']);
+                        return response()->streamDownload(
+                            fn() => print(Excel::download(new \App\Exports\UserExport($data), 'pengguna.xlsx')->getFile()->getContent()),
+                            'laporan-list-data-pengguna.xlsx'
+                        );
+                    }),
 
-                    Excel::import(new UsersImport, $file);
+                Actions\Action::make('import-excel')
+                    ->label('Import Excel')
+                    ->icon('heroicon-o-arrow-down-tray')
+                    ->visible(fn() => auth()->user()->can('pengguna:import'))
+                    ->form([
+                        FileUpload::make('file')
+                            ->label('File Excel')
+                            ->disk('public')
+                            ->visibility("public")
+                            ->directory('imports/users')
+                            ->required()
+                            ->acceptedFileTypes(['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']),
+                    ])
+                    ->action(function (array $data): void {
+                        $file = public_path('storage/' . $data['file']);
+                        Excel::import(new UsersImport, $file);
 
-                    \Filament\Notifications\Notification::make()
-                        ->title('Import berhasil')
-                        ->success()
-                        ->send();
-                }),
+                        \Filament\Notifications\Notification::make()
+                            ->title('Import berhasil')
+                            ->success()
+                            ->send();
+                    }),
+
+                Actions\Action::make('download-template')
+                    ->label('Download Template Excel')
+                    ->icon('heroicon-o-document-arrow-down')
+                    ->visible(fn() => auth()->user()->can('pengguna:import'))
+                    ->action(function () {
+                        return response()->download(
+                            storage_path('app/public/templates/template_import_users.xlsx'),
+                            'template-import-pengguna.xlsx'
+                        );
+                    }),
+            ])
+                ->button()
+                ->label('Opsi Lainnya')
+                ->icon('heroicon-o-cog-6-tooth')
+                ->visible(fn() => auth()->user()->can('pengguna:import') || auth()->user()->can('pengguna:export')),
 
         ];
     }
