ad292d14e5f243c0b7f8e1d84c6467d7055d8c72
MrFatra <ramadhanfatra2004@gmail.com>
2025-08-12 20:46:46 +0700

    fix(header): change tagline text

--- resources/js/sections/Header.jsx
+++ resources/js/sections/Header.jsx
@@ -11,15 +11,16 @@ const Header = () => {
             <div className="max-w-7xl mx-auto px-6 py-32 flex flex-col-reverse lg:flex-row items-center gap-12">
                 {/* Text Section */}
                 <div className="w-full lg:w-1/2 text-center lg:text-left">
-                    <h3 className="text-2xl font-bold text-custom-emerald underline tracking-wide">
-                        {import.meta.env.VITE_APP_NAME.toUpperCase()}
-                    </h3>
-                    <h2 className="text-3xl md:text-4xl xl:text-5xl font-bold mt-4 leading-tight">
-                        Posyandu Muncangela: <br />
-                        <span className="text-shades">
-                            Sehat Warganya, Maju Desanya
-                        </span>
+                    <p className="font-semibold text-xl">siposyandu.com</p>
+                    <h2 className="text-3xl md:text-4xl xl:text-5xl font-bold mt-4 leading-tight text-custom-emerald tracking-wide">
+                        SISTEM INFORMASI POSYANDU
                     </h2>
+                    <h2 className="text-3xl md:text-4xl xl:text-5xl font-bold mt-4 leading-tight text-shades">
+                        DESA MUNCANGELA
+                    </h2>
+                    <p className="mt-4 text-lg md:text-xl italic text-shades font-bold">
+                        "Posyandu Muncangela: Sehat Warganya, Maju Desanya"
+                    </p>
                     <Link
                         href={route("login.index")}
                         className="inline-flex items-center gap-3 mt-8 border-2 border-custom-emerald text-custom-emerald px-6 py-3 rounded-lg font-bold hover:bg-custom-emerald hover:text-white transition-all duration-300"
