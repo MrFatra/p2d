b7219402beba2edafd6674fd8496f2bc8ed0c9ee
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-08-13 01:53:35 +0700

    Fixed: Yearly Export

--- app/Helpers/YearlyReport.php
+++ app/Helpers/YearlyReport.php
@@ -12,12 +12,39 @@ class YearlyReport
 {
     public static function countPerModelByYear(int $year): array
     {
+        $hamlet = auth()->user()->hamlet; // ambil hamlet user login
+
         return [
-            'Elderly'  => Elderly::whereYear('created_at', $year)->count(),
-            'Infant'   => Infant::whereYear('created_at', $year)->count(),
-            'Pregnant' => PregnantPostpartumBreastfeending::whereYear('created_at', $year)->count(),
-            'Teenager' => Teenager::whereYear('created_at', $year)->count(),
-            'Toddler'  => Toddler::whereYear('created_at', $year)->count(),
+            'Elderly'  => Elderly::whereYear('created_at', $year)
+                ->whereHas('user', function ($q) use ($hamlet) {
+                    $q->where('hamlet', $hamlet);
+                })
+                ->count(),
+
+            'Infant'   => Infant::whereYear('created_at', $year)
+                ->whereHas('user', function ($q) use ($hamlet) {
+                    $q->where('hamlet', $hamlet);
+                })
+                ->count(),
+
+            'Pregnant' => PregnantPostpartumBreastfeending::whereYear('created_at', $year)
+                ->whereHas('user', function ($q) use ($hamlet) {
+                    $q->where('hamlet', $hamlet);
+                })
+                ->count(),
+
+            'Teenager' => Teenager::whereYear('created_at', $year)
+                ->whereHas('user', function ($q) use ($hamlet) {
+                    $q->where('hamlet', $hamlet);
+                })
+                ->count(),
+
+            'Toddler'  => Toddler::whereYear('created_at', $year)
+                ->whereHas('user', function ($q) use ($hamlet) {
+                    $q->where('hamlet', $hamlet);
+                })
+                ->count(),
+
             'year'     => $year,
         ];
     }
