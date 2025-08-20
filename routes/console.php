6816a350ed899d85d6c2a980121a36a672fcb260
MrFatra <ramadhanfatra2004@gmail.com>
2025-08-21 06:39:18 +0700

    refactor(scheduler): remove kernel.php & unused artisan command

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('app:update-user-roles')->daily();
 Schedule::command('notify:posyandu')->daily();
