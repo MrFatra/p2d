f5f2c64f8d258276de38c31090b61363bdeb0ae7
MrFatra <ramadhanfatra2004@gmail.com>
2025-09-14 21:41:40 +0700

    style: remove constant raw html

--- app/Filament/Resources/PreschoolerResource.php
+++ app/Filament/Resources/PreschoolerResource.php
@@ -104,7 +104,22 @@ class PreschoolerResource extends Resource
 
                         Placeholder::make('info_alert_note')
                             ->hiddenLabel()
-                            ->content(Constant::renderInfoAlert('Untuk mengubah nama <strong>Ayah</strong> atau <strong>Ibu</strong>, silakan kunjungi halaman <em>Edit Pengguna</em>.'))
+                            ->content(
+                                new HtmlString('
+                                    <div class="rounded-md bg-blue-50 p-4 border border-blue-200">
+                                        <div class="flex items-center text-blue-800">
+                                            <div class="flex-shrink-0">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                                                </svg>
+                                            </div>
+                                            <div class="ml-3 text-sm">
+                                            Untuk mengubah nama <strong>Ayah</strong> atau <strong>Ibu</strong>, silakan kunjungi halaman <em>Edit Pengguna</em>.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    ')
+                            )
                             ->columnSpanFull()
                     ]),
 
