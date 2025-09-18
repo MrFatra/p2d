dc766408201ccffb63286cb3e6488a9d700a38b2
MrFatra <ramadhanfatra2004@gmail.com>
2025-09-18 16:45:05 +0700

    set external link on pdf guidance

--- resources/views/filament/widgets/user-guide-widget.blade.php
+++ resources/views/filament/widgets/user-guide-widget.blade.php
@@ -1,14 +1,16 @@
 <x-filament-widgets::widget>
-    <x-filament::section class="bg-blue-100 border border-blue-500">
+    <x-filament::section class="border border-blue-500" style="background-color: #dbeafe !important">
 
         <div class="flex items-center text-blue-800">
             <div class="text-sm">
                 <h3 class="font-bold text-lg">Panduan Penggunaan Aplikasi</h3>
                 <p class="mt-1">Download panduan lengkap untuk memulai menggunakan aplikasi ini.</p>
-                <a href="{{ Storage::url('guidance/E-Book-SIPOSYANDU-2025.pdf') }}" class="text-blue-600 hover:underline mt-2 flex gap-2 items-center">
+                <a href="{{ Storage::url('guidance/E-Book-SIPOSYANDU-2025.pdf') }}" target="_blank"
+                    rel="noopener noreferrer" class="text-blue-600 hover:underline mt-2 flex gap-2 items-center">
                     @svg('heroicon-o-arrow-down-tray', ['class' => 'w-5 h-5'])
                     Download PDF Panduan
                 </a>
+
             </div>
         </div>
 
